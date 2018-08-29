cb0546db07904eda0d7c42a5c9001bbf1476f0a2
Gidralisk <gidralisk@gmail.com>
2018-08-29 21:57:31 +0300

    ooooooo

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -4,7 +4,7 @@ import Link from 'gatsby-link'
 const SecondPage = () => (
   <div>
     <h1>Hi from the second page</h1>
-    <p>Welcome to page ?? Comit + Sjjjjjjjjjjjave</p>
+    <p>Welcome to page ?? Comit + Sjjjjjjjjjjjjkjhkjhkjhkhkjhkave</p>
     <Link to="/">Go back to the homepage</Link>
   </div>
 )
