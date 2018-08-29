fc143074d0aec956cc6430cc3a1c2eaddec0990a
Gidralisk <gidralisk@gmail.com>
2018-08-29 23:31:15 +0300

    dddd

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ const IndexPage = () => (
   <div>
     <h1>Hi people</h1>
     <p>Welcome to your new</p>
-    <p>Now go buildhomegvvvvvvvvvvvvvpagehomegvvvvvvvvvvvvvpage.</p>
+    <p>Now go buildhomegvvvvvvvvvvvvvpagehomegvvvvvvvvvvvvvpag.</p>
     <Link to="/pgage-2/">Go to page yo!</Link>
   </div>
 )
