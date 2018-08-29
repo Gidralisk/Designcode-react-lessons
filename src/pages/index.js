22023d53da611145301324e6a5e0b00b6e2565f8
Gidralisk <gidralisk@gmail.com>
2018-08-29 21:36:47 +0300

    1111111

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,10 +4,11 @@ import Link from 'gatsby-link'
 const IndexPage = () => (
   <div>
     <h1>Hi people</h1>
-    <p>Welcome to your new 11111.</p>
-    <p>Now go build somethindffffffdddg grdddddeat.</p>
+    <p>Welcome to your new</p>
+    <p>Now go build.</p>
     <Link to="/pgage-2/">Go to page yo!</Link>
   </div>
 )
 
 export default IndexPage
+
