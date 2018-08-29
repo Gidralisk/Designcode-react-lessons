127872355fa2de9e42b387445926896e3833c34b
Gidralisk <gidralisk@gmail.com>
2018-08-29 21:30:14 +0300

    mmm

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,9 +4,9 @@ import Link from 'gatsby-link'
 const IndexPage = () => (
   <div>
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/pgage-2/">Go to page 2ddddgddd2dww1</Link>
+    <p>Welcome to your new Gatsbddddddddy site.</p>
+    <p>Now go build something grdddddeat.</p>
+    <Link to="/pgage-2/">Go to page 2ddddgddd2dcccccffffww1</Link>
   </div>
 )
 
