4ac66da92d829248ea851e757ca2b53ad12ae60f
Gidralisk <gidralisk@gmail.com>
2018-08-31 00:05:31 +0300

    111

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,38 +5,3 @@ module.exports = {
   plugins: ['gatsby-plugin-react-helmet'],
 }
 
-module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.(png|jpg|gif)$/i,
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              limit: 8192
-            }
-          }
-        ]
-      }
-    ]
-  }
-}
-
-
-
-module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.(png|jpg|gif)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {}
-          }
-        ]
-      }
-    ]
-  }
-}
